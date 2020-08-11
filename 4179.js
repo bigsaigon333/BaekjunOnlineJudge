5d110aa5e6c1e8b0db4c8137b917954998e2b927
bigsaigon333 <likepeppermint@gmail.com>
2020-08-11 21:59:12 +0900

    BFS 복습 문제풀이

---
+++ 4179.js
@@ -0,0 +1,88 @@
+/* Date: 2020-08-11 Tue 21:39
+try: 1h 00m 07s >> 겨우 통과
+comments
+1. maze를 q validation 할때 적절히 활용하자. 
+2. dist(fire, escape)의 초기값은 -1로 해두는게 마음 편하다.
+  */
+
+const input = require("fs")
+	.readFileSync("/dev/stdin")
+	.toString()
+	.trim()
+	.split("\n");
+
+const [R, C] = input[0].split(" ").map((v) => Number(v));
+
+const maze = new Array(R).fill(0).map((v, i) => input[i + 1].split(""));
+const fire = new Array(R).fill(0).map(() => new Array(C).fill(-1));
+const escape = new Array(R).fill(0).map(() => new Array(C).fill(-1));
+
+const MX = 1000000 + 5;
+const q1 = new Array(MX);
+const q2 = new Array(MX);
+let head1 = 0;
+let tail1 = 0;
+let head2 = 0;
+let tail2 = 0;
+const dx = [1, 0, -1, 0];
+const dy = [0, 1, 0, -1];
+
+const q1_push = (val) => (q1[tail1++] = val);
+const q1_shift = () => q1[head1++];
+const q1_size = () => tail1 - head1;
+
+const q2_push = (val) => (q2[tail2++] = val);
+const q2_shift = () => q2[head2++];
+const q2_size = () => tail2 - head2;
+
+for (let x = 0; x < R; x++) {
+	for (let y = 0; y < C; y++) {
+		if (maze[x][y] === "F") {
+			q1_push({ x, y });
+			fire[x][y] = 0;
+		} else if (maze[x][y] === "J") {
+			q2_push({ x, y });
+			escape[x][y] = 0;
+		}
+	}
+}
+// console.table(fire);
+// console.table(escape);
+// console.table(q1);
+
+while (q1_size() > 0) {
+	let curr = q1_shift();
+	for (let i = 0; i < 4; i++) {
+		const x = curr.x + dx[i];
+		const y = curr.y + dy[i];
+
+		if (x < 0 || x >= R || y < 0 || y >= C) continue;
+		if (fire[x][y] >= 0 || maze[x][y] === "#") continue;
+
+		fire[x][y] = fire[curr.x][curr.y] + 1;
+		q1_push({ x, y });
+	}
+}
+
+// console.table(fire);
+
+while (q2_size() > 0) {
+	let curr = q2_shift();
+	// console.table(escape);
+	for (let i = 0; i < 4; i++) {
+		const x = curr.x + dx[i];
+		const y = curr.y + dy[i];
+
+		if (x < 0 || x >= R || y < 0 || y >= C) {
+			// console.table(escape);
+			console.log(escape[curr.x][curr.y] + 1);
+			return;
+		}
+		if (maze[x][y] === "#" || escape[x][y] >= 0) continue;
+		if (fire[x][y] !== -1 && escape[curr.x][curr.y] + 1 >= fire[x][y]) continue;
+		escape[x][y] = escape[curr.x][curr.y] + 1;
+		q2_push({ x, y });
+	}
+}
+// console.table(escape);
+console.log("IMPOSSIBLE");
--- 7576.js
+++ 7576.js
@@ -5,6 +5,12 @@ Comments
 1. Javascript Array shift function의 시간복잡도는 O(N)이다. 
 이번 문제에서 최대 N이 1,000이었음에도 불구하고 계속해서 shift()를 하였더니, 시간초과가 발생하였다.
 이럴때는 arr, head, tail 을 가지고 직접 q를 구현하여서 문제를 풀면 된다.
+
+Date: 2020-08-11 Tue 21:57
+3rd try >> 통과
+Comments
+1. BFS 좌표 문제 풀때는 dist로 생각해라. 즉 거리라고 생각해라. 그러면 초기값은 -1이 되어야 논리적이다.
+2/ board를 충분히 활용해라. dist 배열 하나로 모든걸 해결하려고 하지말고 특정 board값에 해당되면 continue 하게끔 해라
  */
 
 const input = require("fs")
@@ -19,7 +25,7 @@ const N = Number(line[1]);
 
 const MX = 1000 + 5;
 const board = [];
-const date = [];
+const date = new Array(N).fill(0).map(() => new Array(M).fill(-1));
 const q = new Array(MX).fill(0);
 let head = 0;
 let tail = 0;
@@ -32,6 +38,13 @@ const q_shift = () => {
 	return q[head++];
 };
 
+const q_print = () => {
+	for (let i = head; i < tail; i++) {
+		console.log("i v", i - head, q[i]);
+	}
+	console.log("");
+};
+
 const q_size = () => tail - head;
 
 const dx = [1, 0, -1, 0];
@@ -40,34 +53,33 @@ const dy = [0, 1, 0, -1];
 for (let i = 1; i <= N; i++) {
 	line = input[i].split(" ").map((v) => Number(v));
 	board.push(line);
-	date.push(line);
 }
 
 for (let i = 0; i < N; i++) {
 	for (let j = 0; j < M; j++) {
 		if (board[i][j] === 1) {
-			// q.push({ x: i, y: j });
 			q_push({ x: i, y: j });
+			date[i][j] = 0;
 		}
 	}
 }
+// console.table(board);
 // console.table(date);
-// console.table(q);
+// q_print();
 
 while (q_size() > 0) {
-	// let cor = q.shift();
+	// console.table(date);
+	// q_print();
 	let cor = q_shift();
-	// console.log(cor);
 	for (let i = 0; i < 4; i++) {
 		const nx = cor.x + dx[i];
 		const ny = cor.y + dy[i];
 
 		if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
-		if (date[nx][ny] === 0) {
-			date[nx][ny] = date[cor.x][cor.y] + 1;
-			// q.push({ x: nx, y: ny });
-			q_push({ x: nx, y: ny });
-		}
+		if (board[nx][ny] === -1 || date[nx][ny] >= 0) continue;
+
+		date[nx][ny] = date[cor.x][cor.y] + 1;
+		q_push({ x: nx, y: ny });
 	}
 }
 // console.table(date);
@@ -75,11 +87,11 @@ while (q_size() > 0) {
 let max = 0;
 for (let i = 0; i < N; i++) {
 	for (let j = 0; j < M; j++) {
-		if (date[i][j] === 0) {
+		if (board[i][j] !== -1 && date[i][j] === -1) {
 			console.log(-1);
 			return;
 		}
 		if (date[i][j] > max) max = date[i][j];
 	}
 }
-console.log(max - 1);
+console.log(max);
