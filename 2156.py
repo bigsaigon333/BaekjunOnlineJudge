5671d3b7b884379de9616247ea6b754b97fbd7eb
bigsaigon333 <likepeppermint@gmail.com>
2020-09-14 23:13:06 +0900

    2156 DP Best Answer 추가

--- 2156.py
+++ 2156.py
@@ -1,6 +1,8 @@
 # Date: 2020-09-14 Mon 23:09
 # 1st try: 45m 58s >> 통과
 # Comment: DP를 잘 알고 있다고 생각했으나 전혀...  DP는 부분해를 구해가며 전체해를 찾아가는 것. 점화식을 잘 찾아야 하는데 쉽지 않네..
+# Best Answer: https://www.acmicpc.net/source/16367968 >> 파이썬 DP 풀이를 보면 유달리 이러한 풀이가 많이 보인다. 아직 이해하기가 쉽진 않은데.. 깔끔해 보이긴 하네
+
 
 import sys
 from itertools import combinations
